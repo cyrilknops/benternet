4a7172aacb34de02ab940718fa0f17db9f5e7a64
cyrilknops <cyrilknops@gmail.com>
2020-06-19 13:01:42 +0200

    you can now list all the ips form a url by clicking list all

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ var BROKER_URL_SUB = "";
 const TOPIC = "DNS";    //the topic that the server listens to
 var numMessages = 0;    // how many messages the server receives (for debug)
 
+//Push code
 function pushMessage(msg) { //push a message to the broker
     const pushsock = new zmq.socket("push");
     try {
@@ -49,6 +50,7 @@ function pushMessage(msg) { //push a message to the broker
     pushsock.disconnect(BROKER_URL_PUSH)
 }
 
+//Get message code
 function getMessage() { //get called every time there is a message
     subsock.on("message", function(topic, message) {
         numMessages++; //for debug
@@ -72,10 +74,11 @@ function getMessage() { //get called every time there is a message
                     //console.log("DNS!>I don't know the ip of", String(msg)); //for debug
                     dns.lookup(String(msg), (err, address, family) => {
                         pushMessage(TOPIC+"!>" + String(msg)+":"+String(address));
-                        con.query("SELECT * FROM DNS WHERE url LIKE" +mysql.escape(msg),(err, result) =>{
+                        con.query("SELECT * FROM DNS WHERE url LIKE" +mysql.escape(msg),(err, result) =>{ //checks if the url is in the dns table
                             if(result.length <= 0) {
-                                con.query("INSERT INTO DNS (url)VALUES(" + mysql.escape(msg) + ")", (err, result) => {
+                                con.query("INSERT INTO DNS (url)VALUES(" + mysql.escape(msg) + ")", (err, result) => { //add the url to the dns table
                                     if (err) throw err;
+                                    //add the ip to the IPS table with the id of the newly created dns record
                                     con.query("INSERT INTO IPS (DNSid, ip) VALUES(" + mysql.escape(result.insertId) + "," + mysql.escape(String(address)) + ")", function (err, result, fields) {
                                         if (err) throw err;
                                         //console.log(result);
@@ -94,7 +97,7 @@ function getMessage() { //get called every time there is a message
                                         }
                                     });
                                 });
-                            }else{
+                            }else{ //if the url allready exists only add the IP to the IPS table with the id of the dns
                                 con.query("INSERT INTO IPS (DNSid, ip) VALUES("+mysql.escape(result[0].id)+","+mysql.escape(String(address))+")",function (err, result, fields) {
                                     if (err) throw err;
                                     //console.log(result);
@@ -121,10 +124,11 @@ function getMessage() { //get called every time there is a message
             let url = String(topic).split(">")[1];
             let ip = String(topic).split(">")[2];
             var IP = false;
-            con.query("SELECT * FROM DNS WHERE url LIKE" +mysql.escape(url),(err, result) =>{
+            con.query("SELECT * FROM DNS WHERE url LIKE" +mysql.escape(url),(err, result) =>{ //check if the url exists in the dns table
                if(result.length <= 0){
-                   con.query("INSERT INTO DNS (url)VALUES("+mysql.escape(url)+")",(err, result)=>{
+                   con.query("INSERT INTO DNS (url)VALUES("+mysql.escape(url)+")",(err, result)=>{ //add url to the dns table
                        if (err) throw err;
+                       //add ip to the IPS table with the newly created id of the dns
                        con.query("INSERT INTO IPS (DNSid, ip) VALUES("+mysql.escape(result.insertId)+","+mysql.escape(String(ip))+")",function (err, result, fields) {
                            if (err) throw err;
                            //console.log(result);
@@ -144,6 +148,7 @@ function getMessage() { //get called every time there is a message
                        });
                    });
                }else{
+                   //if url exists in dns table insert only the ip into the IPS table with the dns id
                    con.query("INSERT INTO IPS (DNSid, ip) VALUES("+mysql.escape(result[0].id)+","+mysql.escape(String(ip))+")",function (err, result, fields) {
                        if (err) throw err;
                        //console.log(result);
@@ -160,7 +165,7 @@ function getMessage() { //get called every time there is a message
                }
             });
 
-        }else if(String(topic).includes(TOPIC+'DELETE?>')) {
+        }else if(String(topic).includes(TOPIC+'DELETE?>')) { //delete a id from the ip table (no need to delete the url in dns record, won't return if there is nog ip)
             console.log(String(topic));
             let url = String(topic).split(">")[1];
             con.query("DELETE FROM IPS WHERE ip LIKE " + mysql.escape(String(url)), function (err, result, fields) {
@@ -169,7 +174,7 @@ function getMessage() { //get called every time there is a message
                     pushMessage(TOPIC+"DELETE!>" + String(url)+":Deleted");
                 }
             });
-        }else if(String(topic).includes(TOPIC+'LB?>')){
+        }else if(String(topic).includes(TOPIC+'LB?>')){ //return a list of all the ips that are in the ip record from a specific url
             let url = String(topic).split(">")[1];
             con.query("SELECT IPS.ip FROM DNS, IPS WHERE DNS.id = IPS.DNSid AND DNS.url LIKE"+mysql.escape(url),(err, results)=>{
                 let listOfIPs = "";
@@ -179,7 +184,7 @@ function getMessage() { //get called every time there is a message
                 console.log(listOfIPs);
                 pushMessage(TOPIC+"LB!>" + String(url)+">"+listOfIPs);
             });
-        }else if(String(topic).includes(TOPIC+'WHOIS?>')){
+        }else if(String(topic).includes(TOPIC+'WHOIS?>')){ //return the whois data from a specific url
             let url = String(topic).split(">")[1];
             getWhois(url);
         }
@@ -203,22 +208,26 @@ getWhois = async function(url){
     ip = country+">"+isp;
     pushMessage(TOPIC+"WHOIS!>" + String(url)+">"+ip);
 };
+
+
 const subsock = new zmq.socket("sub");
 subsock.connect(BROKER_URL_SUB);
 subsock.subscribe(TOPIC);
 console.log("Server started");
 getMessage();
 
+
+//Start API code
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/html/index.html');
 });
 
 app.get('/api', async function(request, response){
     var ip = "";
-    if (request.query.r == 'delete'){
+    if (request.query.r == 'delete'){ //delete a url
         pushMessage(TOPIC+"DELETE?>"+request.query.url);
         response.send("record deleted");
-    } else if (request.query.r == 'add'){
+    } else if (request.query.r == 'add'){ //add a dns record
         if('ip' in request.query){
             ip = request.query.ip;
         }else{
@@ -229,7 +238,7 @@ app.get('/api', async function(request, response){
         }
         pushMessage(TOPIC+"ADD?>"+request.query.url+">"+ip);
         response.send("record added or updated");
-    }else if (request.query.r == 'ask'){
+    }else if (request.query.r == 'ask'){ //search for a dns record(show the one that was first used)
         pushMessage(TOPIC+"?>"+request.query.url);
         con.query("SELECT DNS.url, IPS.ip FROM DNS, IPS WHERE DNS.id = IPS.DNSid AND DNS.url LIKE " + mysql.escape(String(request.query.url))+" ORDER BY lastUsed ASC LIMIT 1", function (err, result, fields) {
             if (err) throw err;
@@ -249,7 +258,31 @@ app.get('/api', async function(request, response){
                 pushMessage(TOPIC+"?>"+request.query.url);
             }
         });
-    }else if(request.query.r == 'whois'){
+    }else if (request.query.r == 'asklist'){ //search for a dns record(show the one that was first used)
+        pushMessage(TOPIC+"?>"+request.query.url);
+        con.query("SELECT DNS.url, IPS.ip FROM DNS, IPS WHERE DNS.id = IPS.DNSid AND DNS.url LIKE " + mysql.escape(String(request.query.url)), function (err, results, fields) {
+            if (err) throw err;
+            if(results.length >= 1) {
+                //console.log(result);
+                let res = "";
+                try {
+                    results.forEach((result)=>{
+                    res = res+">"+result.ip;
+                });
+                    let result = {
+                        ip:res
+                    }
+                    result = JSON.stringify(result);
+                    response.send(result);
+                } catch (e) { //if there is no ip for a provided url
+                    console.log("something went wrong");
+                }
+            }else{
+                pushMessage(TOPIC+"?>"+request.query.url);
+            }
+        });
+    }
+    else if(request.query.r == 'whois'){ //show the whois data
         let url = request.query.url;
         let results = await whois(url);
         console.log(results);
@@ -274,7 +307,7 @@ app.get('/api', async function(request, response){
         response.send(JSON.stringify(res));
     }
     else {
-        con.query("SELECT DNS.url, DNS.id, IPS.ip FROM DNS, IPS WHERE IPS.DNSid = DNS.id", function (err, result, fields) {
+        con.query("SELECT DNS.url, DNS.id, IPS.ip FROM DNS, IPS WHERE IPS.DNSid = DNS.id", function (err, result, fields) { //return all the records
             if (err) throw err;
             //console.log(result);
             try {
